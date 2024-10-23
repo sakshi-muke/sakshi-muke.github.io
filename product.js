24bc2f1a11cb1508960f451f2f0fe6b06094959d
sakshi-muke <sakshimuke028@gmail.com>
2024-10-23 21:42:07 +0530

    Update product.js

--- product.js
+++ product.js
@@ -1,1 +1,9 @@
+// Simple product interaction
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Item added to cart!');
+    });
+});
 
